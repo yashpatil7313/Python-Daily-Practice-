629c97a632702466dc7ad9e2fd2820475b7debad
Yash patil <yashrspatil7313@gmail.com>
2026-01-26 14:54:37 +0530

    Create Program03_AddTwoNumbers.py

---
+++ Program03_AddTwoNumbers.py
@@ -0,0 +1,7 @@
+# Program03_AddTwoNumbers.py
+# This program adds two numbers entered by the user
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+sum_result = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum_result}")
