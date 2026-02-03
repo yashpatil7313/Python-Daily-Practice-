d2c191febb0132654e9b15fc77ff804485e1fee9
Yash patil <yashrspatil7313@gmail.com>
2026-02-03 14:47:12 +0530

    Create program08_largest_of_two.py

---
+++ program08_largest_of_two.py
@@ -0,0 +1,10 @@
+# Program 08: Find the largest of two numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("Largest number:", a)
+else:
+    print("Largest number:", b)
+
