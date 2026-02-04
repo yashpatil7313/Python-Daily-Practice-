4f8c79f45abc0798a67d983525336e39cfcffdf9
Yash patil <yashrspatil7313@gmail.com>
2026-02-04 10:27:23 +0530

    Create program11_sum_of_two_numbers.py

---
+++ program11_sum_of_two_numbers.py
@@ -0,0 +1,14 @@
+# Program 11: Sum of two numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum = a + b
+print("Sum is:", sum)
+
+# second method 
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+
+sum = a +  b
+print(f"sum is {sum}")
